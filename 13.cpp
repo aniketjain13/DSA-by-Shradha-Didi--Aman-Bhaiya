19df790aeb33cee4727b22711e0e15b44e015a8f
codeitaniket <aniketjain836@gmail.com>
2022-07-02 18:30:49 +0530

    Maximum Product Subarray

--- 13.cpp
+++ 13.cpp
@@ -1,5 +1,7 @@
 /*
-Question:- Write a program to reverse an array or string
+Question:- Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+           The test cases are generated so that the answer will fit in a 32-bit integer.
+           A subarray is a contiguous subsequence of the array.
 Solution coded by:- Aniket Jain
 */
 
