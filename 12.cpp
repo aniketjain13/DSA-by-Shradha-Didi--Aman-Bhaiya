e77399a31db8337b45ea2c0f9eccb46e66c13ff4
codeitaniket <aniketjain836@gmail.com>
2022-07-02 18:30:36 +0530

    Product of Array Except Self

--- 12.cpp
+++ 12.cpp
@@ -1,5 +1,7 @@
 /*
-Question:- Write a program to reverse an array or string
+Question:- Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+           The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+           You must write an algorithm that runs in O(n) time and without using the division operation.
 Solution coded by:- Aniket Jain
 */
 
